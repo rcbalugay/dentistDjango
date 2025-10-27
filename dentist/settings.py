00553b238a1870e6a2f3860e40552a6737bc51b8
rcbalugay <37728722+rcbalugay@users.noreply.github.com>
2025-10-27 15:53:19 +0800

    Update settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -109,6 +109,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Whitenoise storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
